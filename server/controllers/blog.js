231a4a3a499dd75d4a60e12fc1ec6f122e13816b
harshitajethani23 <harshitajethani2@gmail.com>
2025-11-13 20:07:38 +0530

    feat: API GET blogs

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -30,4 +30,15 @@ const postBlogs = async (req,res) => {
         blog:savedBlog,
     })
 };
-export {postBlogs};
+
+const getBlogs = async (req,res) => {
+    const blogs = await Blog.find().populate("author","_id name email")
+
+    res.status(200).json({
+        success:true,
+        data:blogs,
+        message:"Blogs fetched successfully",
+    })
+};
+
+export {getBlogs,postBlogs};
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import {postSignup,postLogin} from "./controllers/user.js"
-import {postBlogs} from "./controllers/blog.js";
+import {postBlogs,getBlogs} from "./controllers/blog.js";
 dotenv.config();
 
 const app= express();
@@ -33,6 +33,7 @@ app.get("/",(req,res)=>{
 
 app.post("/signup",postSignup);
 app.post("/login",postLogin);
+app.get("/blogs",getBlogs);
 app.post("/blogs",postBlogs)
 
 
